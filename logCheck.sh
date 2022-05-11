90f7b08e1e182fa09f10ad1c6939037417399cc5
monster663 <1370397561@qq.com>
2022-05-11 09:01:21 +0800

    2022/5/11

---
+++ logCheck.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#author:monster
+#2022/5/11
+echo "#########################################"
+echo "#          linux-secure日志审查系统     #"
+echo "#########################################"
+
+loginSUser=$(cat /var/log/secure | grep "Accepted" | awk '{print $9;}' | uniq)
+loginSUserTimes=$(cat /var/log/secure | grep "Accepted" | awk '{print $11;}' | wc -l)
+loginSUserIP=$(cat /var/log/secure | grep "Accepted" | awk '{print $11;}' | uniq | sort | awk '{print $2,$1}')
+loginNums=$(cat /var/log/secure | grep "Accepted" | wc -l)
+FailedNums=$(cat /var/log/secure | grep "Failed" | wc -l)
+BadIP=$(cat /var/log/secure | grep "Failed password for" | grep -o -E "([0-9]{1,3}\.){3}([0-9]){1,3}" | uniq -u | awk '{a[$1]++} END {for(i in a){print i,a[i]}}' | sort -n -r -k 2)
+BadName=$(cat /var/log/secure | grep "Failed password for"| awk '{print $1,$2,$3,$9,$10,$11}' | uniq -u | awk '{a[$4]++} END {for(i in a){print i,a[i]}}' | sort -n -r -k 2)
+
+echo "成功登入次数："$loginNums
+echo "失败登入次数："$FailedNums
+echo "成功登入的用户："$loginSUser"  登入次数："$loginSUserTimes"  登入IP："$loginSUserIP
+echo "#########################################"
+echo "#          尝试爆破ssh服务的恶意IP地址     #"
+echo "#########################################"
+echo "尝试爆破ssh的恶意IP："
+time=0
+for ip in $BadIP;do
+	let time++
+	if  [ $(($time%2)) -eq 1 ]
+		then
+			echo "恶意IP    "$ip | tr -d '\n'
+	fi
+	if  [ $(($time%2)) -eq 0 ]
+		then
+			echo "   攻击次数 "$ip
+	fi
+	if [ $time -eq 20 ]
+		then
+			break
+	fi
+done
+echo "尝试破解的用户名："
+time=0
+for ip in $BadName;do
+	let time++
+	if  [ $(($time%2)) -eq 1 ]
+		then
+			echo "用户名    "$ip | tr -d '\n'
+	fi
+	if  [ $(($time%2)) -eq 0 ]
+		then
+			echo "   破解次数 "$ip
+	fi
+	if [ $time -eq 20 ]
+		then
+			break
+	fi
+done
--- readme.md
+++ readme.md
@@ -1,5 +1,45 @@
-# Linux日志管理系统
+# Linux登入日志管理系统
 
-Shell脚本程序
+Shell脚本程序监控程序
 
-用于快速检索日志信息
+主要用于监控 /var/log/secure 登入日志文件，检索成功登入的IP和尝试恶意破解的IP
+
+持续更新~
+
+## 使用说明
+
+首先将shell脚本放入某个目录下，并使用chmod赋权
+
+![image-20220511084823285](readme.assets/image-20220511084823285.png)
+
+运行脚本程序
+
+![image-20220511085104400](readme.assets/image-20220511085104400.png)
+
+登入IP每个IP表示一个登入地址，下面的按照攻击破解ssh口令的次数排序恶意IP的地址：
+
+![image-20220511085204848](readme.assets/image-20220511085204848.png)
+
+### 常态化运维管理
+
+可以使用crontab设置定时任务进行常态化运维管理
+
+```
+crontab -e
+```
+
+编辑任务如下，每日0点0分时指向命令，这里需要写入你自己服务器的目录
+
+```
+cd /root/logCheck && echo >> login.log && date >> login.log && ./logCheck.sh >> login.log
+```
+
+![image-20220511085714506](readme.assets/image-20220511085714506.png)
+
+执行命令后将在设定目录下生成新的日志文件
+
+![image-20220511085914812](readme.assets/image-20220511085914812.png)
+
+通过检索日志文件可以快速锁定恶意IP，并检查服务器是否被破解
+
+![image-20220511090022351](readme.assets/image-20220511090022351.png)
