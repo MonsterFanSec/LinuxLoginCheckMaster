ca7c313c4a7edbbe65ab3c0f8de84d2771bf1531
monster663 <1370397561@qq.com>
2022-05-12 20:18:13 +0800

    2022/5/12

--- logCheck.sh
+++ logCheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #author:monster
-#2022/5/11
+#2022/5/12
 echo "#########################################"
 echo "#          linux-secure日志审查系统     #"
 echo "#########################################"
@@ -13,9 +13,34 @@ FailedNums=$(cat /var/log/secure | grep "Failed" | wc -l)
 BadIP=$(cat /var/log/secure | grep "Failed password for" | grep -o -E "([0-9]{1,3}\.){3}([0-9]){1,3}" | uniq -u | awk '{a[$1]++} END {for(i in a){print i,a[i]}}' | sort -n -r -k 2)
 BadName=$(cat /var/log/secure | grep "Failed password for"| awk '{print $1,$2,$3,$9,$10,$11}' | uniq -u | awk '{a[$4]++} END {for(i in a){print i,a[i]}}' | sort -n -r -k 2)
 
+cat /var/log/secure | grep "Accepted" | awk '{print $11;}' | uniq | sort | awk '{print $2,$1}'>login_success
+cat /var/log/secure | grep "Failed password for" | grep -o -E "([0-9]{1,3}\.){3}([0-9]){1,3}" | uniq -c | awk '{a[$2]++} END {for(i in a){print i,a[i]}}' | sort -n -r -k 2 > badIP
+cat /var/log/secure  | grep "Failed password\|Accepted password" > ssh_login_file 
+
 echo "成功登入次数："$loginNums
 echo "失败登入次数："$FailedNums
-echo "成功登入的用户："$loginSUser"  登入次数："$loginSUserTimes"  登入IP："$loginSUserIP
+echo "成功登入的用户："$loginSUser"  登入次数："$loginSUserTimes" "
+echo "成功登入IP："
+for line in `cat login_success`  #for each line
+do
+	echo $line
+done
+
+for line1 in `cat login_success`
+do
+	if cat badIP |grep $line1 > /dev/null
+	then
+		echo "*** 警告：SSH服务已被破解 ***"
+		inf=$(cat ssh_login_file | grep $line1 | grep "Accepted")
+		user=$(echo $inf | awk -F" " '{print "ip:"$11 ":"$13"\t\t time: "$1,$2,$3"\t user:" $9 "\t\t state:Accepted"}')
+		echo "详细信息   "$user
+		echo "***************************"
+		echo ""
+	fi
+done
+
+
+
 echo "#########################################"
 echo "#          尝试爆破ssh服务的恶意IP地址     #"
 echo "#########################################"
@@ -53,4 +78,10 @@ for ip in $BadName;do
 			break
 	fi
 done
-	
+
+rm -rf login_success
+rm -rf badIP
+rm -rf ssh_login_file
+
+
+
--- readme.md
+++ readme.md
@@ -6,19 +6,33 @@ Shell脚本程序监控程序
 
 持续更新~
 
+2022/5/12 更新 SSH破解检测，检测成功破解SSH服务的恶意IP信息
+
 ## 使用说明
 
 首先将shell脚本放入某个目录下，并使用chmod赋权
 
 ![image-20220511084823285](readme.assets/image-20220511084823285.png)
 
-运行脚本程序
+运行脚本程序，可以看到破解次数前5的恶意IP和用户名，若服务器被成功破解，则显示成功破解信息，这里需要审查恶意IP是否为合法的用户
 
-![image-20220511090336792](readme.assets/image-20220511090336792.png)
+![image-20220512195818356](readme.assets/image-20220512195818356.png)
 
-登入IP每个IP表示一个登入地址，下面的按照攻击破解ssh口令的次数排序恶意IP的地址：
+程序运行后会在当前目录下生成恶意IP列表 badIP文件，按照破解次数降序排序
 
-![image-20220511085204848](readme.assets/image-20220511085204848.png)
+![image-20220512200409035](readme.assets/image-20220512200409035.png)
+
+查看SSH黑名单配置文件
+
+![image-20220512201058376](readme.assets/image-20220512201058376.png)
+
+如果有需要可以加入SSH黑名单来禁止这些IP访问SSH，建议先备份
+
+![image-20220512201200344](readme.assets/image-20220512201200344.png)
+
+
+
+
 
 ### 常态化运维管理
 
@@ -42,4 +56,4 @@ cd /root/logCheck && echo >> login.log && date >> login.log && ./logCheck.sh >> 
 
 通过检索日志文件可以快速锁定恶意IP，并检查服务器是否被破解
 
-![image-20220511090022351](readme.assets/image-20220511090022351.png)
+![image-20220512201513915](readme.assets/image-20220512201513915.png)
